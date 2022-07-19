c80fb88a75c21c12cc1551f791c3b06158102805
sovisrushain <sovisrushain@gmail.com>
2022-07-19 12:24:08 +0530

    (:=)

---
+++ 002-variables/var_03.go
@@ -0,0 +1,16 @@
+//short declaration operator - declare and initialise creating a statement (:=)
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	fmt.Println(x)
+
+	x = 99
+	fmt.Println(x)
+
+	y := 50 + 30
+	fmt.Println(y)
+}
